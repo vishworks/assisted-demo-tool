02a5a5033c5b583973b3d45f8ee67d1fac552da0
Alessio Scalici <alessio.scalici@gmail.com>
2018-06-06 15:36:18 +0200

    setting the correct persona on first loaded step

--- src/state/config/operations.js
+++ src/state/config/operations.js
@@ -1,79 +1,93 @@
-import { find, first, toInteger, forEach, get } from 'lodash';
+import { find, toInteger, forEach, get } from 'lodash';
 import { setGlobalError } from '../ui/actions.js';
 import { parseCurrentHash } from '../../helpers/HashUtils.js';
 import validateConfig from 'helpers/ConfigValidator.js';
 import { loadConfig } from './actions.js';
+import { getDemosConfigData } from 'state/demos/selectors.js';
+
+const demoIsIncluded = (demosConfigData, demoId) =>
+  !demosConfigData ||
+  (demosConfigData[demoId] && demosConfigData[demoId].included);
 
 export function asyncLoadConfig(configUrl) {
-  return function(dispatch) {
-    return setTimeout(() => {
-      fetch(configUrl).then(
-        response => {
-          try {
-            response.json().then(function(data) {
-              if (!validateConfig(data)) {
-                let errors = validateConfig.errors,
-                  errorMsg = 'Errors in configuration:\n\n';
+  return function(dispatch, getState) {
+    // return setTimeout(() => {
+    fetch(configUrl).then(
+      response => {
+        try {
+          response.json().then(function(data) {
+            if (!validateConfig(data)) {
+              let errors = validateConfig.errors,
+                errorMsg = 'Errors in configuration:\n\n';
 
-                forEach(errors, err => {
-                  errorMsg +=
-                    'config' +
-                    err.dataPath +
-                    ' ' +
-                    err.message +
-                    ' (found: ' +
-                    JSON.stringify(get(data, err.dataPath.substring(1))) +
-                    ')\n\n';
-                });
-                dispatch(setGlobalError(errorMsg));
-                return;
-              }
+              forEach(errors, err => {
+                errorMsg +=
+                  'config' +
+                  err.dataPath +
+                  ' ' +
+                  err.message +
+                  ' (found: ' +
+                  JSON.stringify(get(data, err.dataPath.substring(1))) +
+                  ')\n\n';
+              });
+              dispatch(setGlobalError(errorMsg));
+              return;
+            }
 
-              // select currently selected state after loadConfig
-              let { demoId, stepNumber, personaId } = parseCurrentHash();
-              let selectedDemoId, selectedStepIndex, selectedPersonaId;
+            // select currently selected state after loadConfig
+            const { demoId, stepNumber, personaId } = parseCurrentHash();
+            const demosConfigData = getDemosConfigData(getState());
+            console.log(demosConfigData);
 
-              selectedDemoId =
-                demoId && find(data.demos, { id: demoId })
-                  ? demoId
-                  : first(data.demos).id;
-
-              selectedStepIndex = !!stepNumber ? toInteger(stepNumber) - 1 : 0;
-
-              selectedPersonaId =
-                personaId && find(data.personas, { id: personaId })
-                  ? personaId
-                  : first(data.personas).id;
-
-              dispatch(
-                loadConfig(
-                  data,
-                  selectedDemoId,
-                  selectedStepIndex,
-                  selectedPersonaId
-                )
+            // the selected demo:
+            // - must exist in the demo list
+            // - if a configuration is present, check if it's included. If not pick the first included demo
+            const selectedDemo =
+              (demoId &&
+                demoIsIncluded(demosConfigData, demoId) &&
+                find(data.demos, { id: demoId })) ||
+              find(data.demos, demo =>
+                demoIsIncluded(demosConfigData, demo.id)
               );
-            });
-          } catch (er) {
+
+            const selectedDemoId = selectedDemo.id;
+
+            const selectedStepIndex = !!stepNumber
+              ? toInteger(stepNumber) - 1
+              : 0;
+
+            const selectedPersonaId =
+              personaId && find(data.personas, { id: personaId })
+                ? personaId
+                : selectedDemo.steps[selectedStepIndex].personaId;
+
             dispatch(
-              setGlobalError(
-                "Error parsing configuration JSON at '" +
-                  configUrl +
-                  "': " +
-                  er.message
+              loadConfig(
+                data,
+                selectedDemoId,
+                selectedStepIndex,
+                selectedPersonaId
               )
             );
-            console.error(er);
-          }
-        },
-        error => {
+          });
+        } catch (er) {
           dispatch(
             setGlobalError(
-              "Failed to fetch configuration at '" + configUrl + "'"
+              "Error parsing configuration JSON at '" +
+                configUrl +
+                "': " +
+                er.message
             )
           );
+          console.error(er);
         }
-      );
-    }, 500); // FIXME remove throttling
+      },
+      error => {
+        dispatch(
+          setGlobalError("Failed to fetch configuration at '" + configUrl + "'")
+        );
+      }
+    );
+    // }, 500); // FIXME remove throttling
   };
 }
